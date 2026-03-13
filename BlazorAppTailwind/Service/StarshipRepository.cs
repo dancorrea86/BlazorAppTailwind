4f6fa049d902c2354d03dbb73e47893f9b276089
Daniel <danielmacielcorrea@gmail.com>
2026-03-13 00:48:23 -0300

    Cria processo de salvar dados formulário.

---
+++ BlazorAppTailwind/Service/StarshipRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using BlazorAppTailwind.Models;
+
+namespace BlazorAppTailwind.Service
+{
+    public class StarshipRepository
+    {
+        public static void SaveStarship(Starship starship)
+        {
+            string filePath = "products.json";
+
+            JsonSerializerOptions options = new JsonSerializerOptions { WriteIndented = true };
+            string? json = JsonSerializer.Serialize(starship, options);
+
+            File.WriteAllText(filePath, json);
+        }
+    }
+}
