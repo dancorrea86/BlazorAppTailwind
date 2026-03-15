fa7d655369e797eb87ba2c8c48eb1f3ddb5dcdf9
Daniel <danielmacielcorrea@gmail.com>
2026-03-15 16:38:33 -0300

    Inclusão de ação de exclusão da nave.

--- BlazorAppTailwind/Service/StarshipRepository.cs
+++ BlazorAppTailwind/Service/StarshipRepository.cs
@@ -1,5 +1,5 @@
-﻿using System.Text.Json;
-using BlazorAppTailwind.Models;
+﻿using BlazorAppTailwind.Models;
+using System.Text.Json;
 
 namespace BlazorAppTailwind.Service
 {
@@ -9,8 +9,35 @@ namespace BlazorAppTailwind.Service
         {
             string filePath = "products.json";
 
+            string jsonString = File.ReadAllText(filePath);
+
+            List<Starship> starships;
+
+            if (string.IsNullOrWhiteSpace(jsonString))
+            {
+                starships = new List<Starship>();
+            }
+            else
+            {
+                starships = JsonSerializer.Deserialize<List<Starship>>(jsonString) ?? new List<Starship>();
+            }
+             
+
+            if (starships.Any())
+            {
+                int lastID = starships.Any() ? starships.Max(x => x.ID) : 0;
+   
+                starship.ID = lastID + 1;
+            }
+            else
+            {
+                starship.ID = 0;
+            }
+
+            starships.Add(starship);
+
             JsonSerializerOptions options = new JsonSerializerOptions { WriteIndented = true };
-            string? json = JsonSerializer.Serialize(starship, options);
+            string? json = JsonSerializer.Serialize(starships, options);
 
             File.WriteAllText(filePath, json);
         }
@@ -22,7 +49,42 @@ namespace BlazorAppTailwind.Service
 
             string jsonString = File.ReadAllText(jsonFilePath);
 
+            List<Starship> starships;
+
+            if (string.IsNullOrWhiteSpace(jsonString))
+            {
+                starships = new List<Starship>();
+            }
+            else
+            {
+                starships = JsonSerializer.Deserialize<List<Starship>>(jsonString) ?? new List<Starship>();
+            }
+
+            return starships;
+        }
+
+        public static List<Starship> DeleteStarships(Starship ship)
+        {
+
+            string jsonFilePath = "products.json";
+
+            string jsonString = File.ReadAllText(jsonFilePath);
+
             List<Starship> starships = JsonSerializer.Deserialize<List<Starship>>(jsonString);
+
+            foreach (Starship starship in starships)
+            {
+                if (starship.ID == ship.ID)
+                {
+                    starships.Remove(starship);
+                    break;
+                }
+            }
+
+            JsonSerializerOptions options = new JsonSerializerOptions { WriteIndented = true };
+            string? json = JsonSerializer.Serialize(starships, options);
+
+            File.WriteAllText(jsonFilePath, json);
 
             return starships;
         }
